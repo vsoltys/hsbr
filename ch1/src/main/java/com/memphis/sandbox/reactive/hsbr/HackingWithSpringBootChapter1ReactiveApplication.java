02a6ebbb20da527d2386fc90d2ff6c12efd89b85
vsoltys <vsoltys.dev@gmail.com>
2021-02-05 14:57:16 +0100

    initial commit

---
+++ ch1/src/main/java/com/memphis/sandbox/reactive/hsbr/HackingWithSpringBootChapter1ReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.memphis.sandbox.reactive.hsbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingWithSpringBootChapter1ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingWithSpringBootChapter1ReactiveApplication.class, args);
+	}
+
+}
---
+++ ch1/src/test/java/com/memphis/sandbox/reactive/hsbr/HackingWithSpringBootChapter1ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memphis.sandbox.reactive.hsbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingWithSpringBootChapter1ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
